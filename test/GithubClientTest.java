9e972de2ce82076479dde1a41c2eae383ad576ef
Artem Medeu <artem.medeu@gmail.com>
2011-01-07 15:52:52 +0600

    Test modifications

--- test/GithubClientTest.java
+++ test/GithubClientTest.java
@@ -1,16 +1,11 @@
-import static org.junit.Assert.*;
-
 import java.util.List;
 
-import models.Repo;
 import models.Sheet;
 
 import org.junit.Test;
 
 import play.test.FunctionalTest;
-
 import client.GithubClient;
-
 
 public class GithubClientTest extends FunctionalTest {
 
@@ -18,15 +13,11 @@ public class GithubClientTest extends FunctionalTest {
     public void loadRemoteRepo() throws Exception {
         String userName = "armed";
         String repoName = "test-repo";
-        
+
         List<Sheet> sheets = new GithubClient(userName, repoName).getSheets();
-        
+
         assertNotNull(sheets);
-        
+
         assertEquals(26, sheets.size());
-        
-        for (Sheet sheet : sheets) {
-            System.out.println(sheet.data);
-        }
     }
 }
